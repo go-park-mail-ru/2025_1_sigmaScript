e2ecbb04d38ed2ae642f01e5e6434720d742294a
SkorikovGeorge <skorikovgk@yandex.ru>
2025-03-03 23:42:17 +0300

    fixed all the style mistakes

---
+++ internal/server/mocks/collections.go
@@ -0,0 +1,40 @@
+package mocks
+
+type Film struct {
+	ID         int    `json:"id"`
+	Title      string `json:"title"`
+	PreviewURL string `json:"previewURL"`
+}
+
+type Collection map[int]Film
+
+var BestOfAllTime = Collection{
+	0: {ID: 1, Title: "Бойцовский клуб", PreviewURL: "/img/0.webp"},
+	1: {ID: 2, Title: "Тёмный рыцарь", PreviewURL: "/img/1.webp"},
+	2: {ID: 3, Title: "Форрест Гамп", PreviewURL: "/img/2.webp"},
+	3: {ID: 4, Title: "Крестный отец", PreviewURL: "/img/3.webp"},
+	4: {ID: 5, Title: "Интерстеллар", PreviewURL: "/img/4.webp"},
+	5: {ID: 6, Title: "Криминальное чтиво ", PreviewURL: "/img/5.webp"},
+	6: {ID: 7, Title: "Побег из Шоушенка", PreviewURL: "/img/6.webp"},
+	7: {ID: 8, Title: "Матрица", PreviewURL: "/img/7.webp"},
+	8: {ID: 9, Title: "Зелёная миля", PreviewURL: "/img/8.webp"},
+	9: {ID: 10, Title: "Одержимость", PreviewURL: "/img/9.webp"},
+}
+
+var OskarNominees = Collection{
+	0: {ID: 10, Title: "Ford против Ferrari", PreviewURL: "/img/10.webp"},
+	1: {ID: 11, Title: "Оппенгеймер", PreviewURL: "/img/11.webp"},
+	2: {ID: 12, Title: "Звёздные войны: Эпизод 4 – Новая надежда", PreviewURL: "/img/12.webp"},
+	3: {ID: 13, Title: "Рокки", PreviewURL: "/img/13.webp"},
+	4: {ID: 14, Title: "Джокер", PreviewURL: "/img/14.webp"},
+	5: {ID: 15, Title: "Игра в имитацию ", PreviewURL: "/img/15.webp"},
+	6: {ID: 16, Title: "Начало", PreviewURL: "/img/16.webp"},
+	7: {ID: 17, Title: "Назад в будущее", PreviewURL: "/img/17.webp"},
+	8: {ID: 18, Title: "Гладиатор", PreviewURL: "/img/18.webp"},
+	9: {ID: 19, Title: "Титаник", PreviewURL: "/img/19.webp"},
+}
+
+var Collections = map[string]Collection{
+	"BestOfAllTime": BestOfAllTime,
+	"OskarNominees": OskarNominees,
+}

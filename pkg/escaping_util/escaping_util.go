891a7028db11d2c62493dc4ca69d833d5c90024b
ned8800 <tchichikin@mail.ru>
2025-04-17 15:28:27 +0300

    added escaping util tests

--- pkg/escaping_util/escaping_util.go
+++ pkg/escaping_util/escaping_util.go
@@ -3,25 +3,32 @@ package escapingutil
 import (
 	"errors"
 	"html"
+	"strings"
 )
 
 const (
-	DEFAULT_TEXT_MAX_LENGTH = 500
+	DEFAULT_TEXT_MAX_LENGTH = int64(500)
 )
 
 var (
-	ErrorMaxLength = errors.New("text exeeds the allowed maximum length")
+	ErrorMaxLength            = errors.New("text exeeds the allowed maximum length")
+	ErrorEmptyOrInvalidString = errors.New("text string is null or contains only invalid symbols")
 )
 
-func ValidateInputTextData(textData string, textMaxLength ...int) (string, error) {
-	maxLength := 1
+func ValidateInputTextData(textData string, textMaxLength ...int64) (string, error) {
+	maxLength := DEFAULT_TEXT_MAX_LENGTH
 	if len(textMaxLength) > 0 && textMaxLength[0] > 0 {
 		maxLength = textMaxLength[0]
 	}
 
-	if len(textData) > maxLength {
+	if int64(len(textData)) > maxLength {
 		return "", ErrorMaxLength
 	}
+	trimmedDataString := html.EscapeString(strings.TrimSpace(textData))
 
-	return html.EscapeString(textData), nil
+	if len(trimmedDataString) < 1 {
+		return "", ErrorEmptyOrInvalidString
+	}
+
+	return trimmedDataString, nil
 }
